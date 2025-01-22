f78d6250e786517132b6d8e5eadcc69cd102d0f1
Azamqulov Musurmon <140374591+Azamqulov@users.noreply.github.com>
2025-01-22 20:14:30 +0500

    send

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <header>
+    <h1 class="bg-slate-600">Hello world</h1>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav></nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
